c5e5bb1dcf344f27cdac4739e31ef9bbbf9c8eee
Adam <adamsmalyska@gmail.com>
2025-07-30 13:19:57 +0200

    Works good with edge case

--- events/1984.js
+++ events/1984.js
@@ -3,22 +3,35 @@ const { fuckingChannel } = require('../config.json');
 
 const badWord = /\bf+u+c+k+/i;
 
+function isLink(text) {
+    return text.startsWith('http://') || text.startsWith('https://');
+}
+
+function emojisOnly(text) {
+    const customEmojiRegex = /<a?:\w+:\d+>/g;
+    const unicodeEmojiRegex = /(\p{Emoji_Presentation}|\p{Emoji}\uFE0F|\p{Emoji_Modifier_Base}\p{Emoji_Modifier})/gu;
+    text = text.replace(customEmojiRegex, '').replace(unicodeEmojiRegex, '').trim();
+    console.log(`Emojis only check: "${text}"`);
+    return text.length === 0;
+}
+
+
 module.exports = {
     name: Events.MessageCreate,
     execute(message) {
-        // Ignoruj wiadomości od botów
-        if (message.author.bot) return;
-
-        // Sprawdź, czy kanał nie jest na liście ignorowanych
-        if (fuckingChannel.includes(message.channel.id)) {
+        if (message.channel.id == fuckingChannel) {
+            if (!badWord.test(message.content) && !isLink(message.content) && !emojisOnly(message.content)) {
+                // Reakcja bota
+                message.reply(`https://tenor.com/view/1984-gif-19260546`).catch(console.error);
+                console.log(`XD "${message.channel.name}" od użytkownika "${message.author.tag}".`);
+            }
             return;
         }
-
-        // Sprawdź, czy treść wiadomości zawiera zdefiniowane słowo (ignorując wielkość liter)
-        if (badWord.test(message.content)) {
-            // Reakcja bota
-            message.reply(`Wykryto słowo!`).catch(console.error);
+        else {
+          if (badWord.test(message.content) && !isLink(message.content) && !emojisOnly(message.content)) {
+            message.reply(`https://tenor.com/view/1984-gif-19260546`).catch(console.error);
             console.log(`Wykryto słowo na kanale "${message.channel.name}" od użytkownika "${message.author.tag}".`);
+          }
         }
     },
 };
